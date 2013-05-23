774c91dce32c30e6d88fe1ad4850795a855410af
Fee <fee.braun@hotmail.com>
2013-05-23 10:20:04 +0200

    test

--- GildedRoseWithTest/src/main/java/gildedrose/inheritance/AgedBrie.java
+++ GildedRoseWithTest/src/main/java/gildedrose/inheritance/AgedBrie.java
@@ -1,5 +1,5 @@
 package gildedrose.inheritance;
-
+//test
 public class AgedBrie extends ItemWithBehaviour {
 	public AgedBrie(String name, int sellIn, int quality) {
 		super(name, sellIn, quality);
